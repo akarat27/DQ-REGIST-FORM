2eca9d76fd53cdf91b2e9ecd1a95793540eb4b6b
akarat <akrs33@gmail.com>
2023-10-15 20:01:43 +0700

    Open google

--- src/LoginSuccessPage.js
+++ src/LoginSuccessPage.js
@@ -4,6 +4,12 @@ function LoginSuccessPage() {
   return (
     <div className="thank-you-page">
       <h2>Thank You for Logging In!</h2>
+      <p>
+            {/* By submitting this form, you agree to our{' '} */}
+            <a href={'https://www.google.com'} target="_blank" rel="noopener noreferrer">
+              Open https://www.google.com
+            </a>
+      </p>
     </div>
   );
 }
--- src/ThankYou.js
+++ src/ThankYou.js
@@ -5,6 +5,12 @@ function ThankYou() {
     <div>
       <h2>Thank you for your information!</h2>
       <p>Your data has been submitted successfully.</p>
+      <p>
+            {/* By submitting this form, you agree to our{' '} */}
+            <a href={'https://www.google.com'} target="_blank" rel="noopener noreferrer">
+              Open https://www.google.com
+            </a>
+      </p>
     </div>
   );
 }
